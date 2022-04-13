9ca85c020ce9369167ad40230e59fe978b7da96b
polyomi <danieltrantrandaniel@gmail.com>
2022-04-12 23:19:38 -0600

    Updates

---
+++ precache-manifest.8624a0fc438bf6f06dc2d0baae3eab9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ef99203afed4e7264fe0983d4a85193",
+    "url": "/firegram/index.html"
+  },
+  {
+    "revision": "a98bcbb6ace6eaa9b432",
+    "url": "/firegram/static/css/main.10f85eb4.chunk.css"
+  },
+  {
+    "revision": "aac37b74028281c93aed",
+    "url": "/firegram/static/js/2.b25b1da8.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/firegram/static/js/2.b25b1da8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a98bcbb6ace6eaa9b432",
+    "url": "/firegram/static/js/main.a3d4262c.chunk.js"
+  },
+  {
+    "revision": "a20b20e63cdcd0ca9e81",
+    "url": "/firegram/static/js/runtime-main.9968160c.js"
+  }
+]);
